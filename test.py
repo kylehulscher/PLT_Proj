17cad0a0228d5092983029fdf0f65171a671cdce
Kyle Hulscher <kylehulscher515@gmail.com>
2023-03-31 14:13:42 -0500

    initialize

---
+++ test.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import csv
+
+def load_db_csv():
+    siteList = []
+    with open('data-broker.csv', newline='') as csvfile:
+        csvReader = csv.reader(csvfile, delimiter=',')
+        for row in csvReader:
+            siteList.append(row[2])
+    return siteList
+
+def test_eight_components():
+    driver = webdriver.Chrome()
+
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+    title = driver.title
+    print(title)
+    assert title == "Web form"
+
+    driver.implicitly_wait(0.5)
+
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+    text_box.send_keys("Selenium")
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    print(value)
+    assert value == "Received!"
+
+    driver.quit()
+
+if __name__ == "__main__":
+    #test_eight_components()
+    sl = load_db_csv()
+    print(str(sl))
