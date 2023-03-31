28ce794ed2764f10cca3f2a85601be9ae390033f
Kyle Hulscher <kylehulscher515@gmail.com>
2023-03-31 16:00:29 -0500

    basic search for DNSMPI. Need to make more robust and collect more interesting information for analysis and future reference.

--- test.py
+++ test.py
@@ -21,17 +21,26 @@ def checkForDNSMPI():
 	driver = webdriver.Chrome()
 	sl = loadDBCSV()
 	i = 0
-	for dbURL in sl:
-		print("dbURL #" + str(i) + ": " + dbURL)
-		try:
-			driver.get(dbURL)
-			get_source = driver.page_source
-			search_text = "Do Not Sell My Personal Information"
-			print(search_text in get_source)
-		except:
-			print("An exception occured when loading: " + dbURL)
-		print()
-		i += 1
+	with open('hasDNSMPI.csv', 'w', newline='') as writeFile:
+		fieldnames = ['urlNum', 'urlStr', 'hasDNSMPI']
+		writer = csv.DictWriter(writeFile, fieldnames=fieldnames)
+
+		writer.writeheader()
+		for dbURL in sl:
+			print("dbURL #" + str(i) + ": " + dbURL)
+			try:
+				driver.get(dbURL)
+				get_source = driver.page_source
+				# TODO: Need to make page search more robust to catch various terms/cases etc
+				search_text = "Do Not Sell My Personal Information"
+				hDNSMPI = search_text in get_source
+				print(hDNSMPI)
+				writer.writerow({'urlNum': i, 'urlStr': dbURL, 'hasDNSMPI': hDNSMPI})
+			except:
+				print("An exception occured when loading: " + dbURL)
+				writer.writerow({'urlNum': i, 'urlStr': dbURL, 'hasDNSMPI': 'Err'})
+			print()
+			i += 1
 	driver.quit()
 
 if __name__ == "__main__":
